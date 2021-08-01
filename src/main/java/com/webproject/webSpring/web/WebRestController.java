099e9bc6ba7e68e9b60776df54755478168e5e92
tenny2188 <tenny210@gmail.com>
2021-08-01 17:56:12 +0900

    Web SpringBoot db

--- src/main/java/com/webproject/webSpring/web/WebRestController.java
+++ src/main/java/com/webproject/webSpring/web/WebRestController.java
@@ -1,13 +1,28 @@
 package com.webproject.webSpring.web;
 
+import com.webproject.webSpring.domain.posts.PostRepository;
+import com.webproject.webSpring.domain.posts.Posts;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+    private PostRepository postRepository;
     @GetMapping("/hello")
     public String hello(){
         return "Hello World!";
     }
 
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postRepository.save(dto.toEntity());
+    }
 }
+
---
+++ src/test/java/com/webproject/webSpring/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.webproject.webSpring.domain.posts;
+
+
+import org.junit.After;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @After
+    public void cleanup() {postRepository.deleteAll();}
+
+    /*
+    @Test
+    public void call_Post() {
+        //given
+        postRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("Test Body 바디")
+                .author("tenny210@gmail.com")
+                .build());
+        //when
+        List<Posts> postsList = postRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 게시글"));
+        assertThat(posts.getContent(), is("Test Body 바디"));
+    }
+    */
+    @Test
+    public void BaseTimeEntityADD(){
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("Test Body 바디")
+                .author("tenny210@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
+    }
+
+}
